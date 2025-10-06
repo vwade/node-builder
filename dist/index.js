fa85dc3c41cd3e84787be01d7e81b9c9a978df1d
Victor Wade <44330039+vwade@users.noreply.github.com>
2025-10-06 15:33:19 -0500

    docs: add project readme

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,8 @@
+// src/index.ts
+function create_placeholder(config) {
+  return { ...config };
+}
+
+export { create_placeholder };
+//# sourceMappingURL=index.js.map
 //# sourceMappingURL=index.js.map
